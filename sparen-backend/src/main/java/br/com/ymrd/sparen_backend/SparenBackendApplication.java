29cbabf74763de136ff59ed6ce468fd28b8fdae7
Yuri Dirickson <yuri.dirickson@usp.br>
2025-05-18 17:25:15 -0300

    Criando estrutura basica

---
+++ sparen-backend/src/main/java/br/com/ymrd/sparen_backend/SparenBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ymrd.sparen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparenBackendApplication.class, args);
+	}
+
+}
---
+++ sparen-backend/src/test/java/br/com/ymrd/sparen_backend/SparenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ymrd.sparen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
